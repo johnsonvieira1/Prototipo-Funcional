9fe868af99cd798c65fdcdea4ee2d0fcaed192c9
Johnson Vieira <johnsonvieira1@gmail.com>
2020-04-27 09:58:58 -0300

    Versões
    Atualizando versões das bibliotecas.

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,7 +1,7 @@
 	<!-- CDN Jquery -->
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 	<!-- Bootstrap -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </body>
 </html>
--- inc/header.inc.php
+++ inc/header.inc.php
@@ -14,11 +14,11 @@
 	<!-- fonte -->
 
 	<!-- font awesome -->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css">
 
 	<!-- bootstrap -->
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 	
 	<!-- favicon -->
 	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
